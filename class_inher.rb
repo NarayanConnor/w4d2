02f1150ad04c9393b21e7eaf1b2163a9863c4aaa
mordes89 <mordes89@gmail.com>
2021-07-06 15:40:26 -0400

    340

--- class_inher.rb
+++ class_inher.rb
@@ -28,20 +28,34 @@ class Manager<Employee
     end
 
    
-    def bonus(multi,employee)
-        sal=employee.emp_collect
-        sal.sum*multi
+    def bonus(multi)
+        sal = employee.my_flatten
+        sal.sum * multi
     end
 
- 
-    def emp_collect
-        sal=[]
-        self.each do |emp|
-            return emp.salary if emp.employee.nil?
-            sal + emp.salary + emp_collect(emp)
+    def self.my_flatten
+        sal = []
+        employee.each do el
+            if el.employee.is a?(Array)
+                sal.concat(el.my_flatten)
+            else
+                sal << el.salary
+            end
         end
         sal
     end
+
+  
+    
+ 
+    # def emp_collect
+    #     # sal=[]
+    #     # self.each do |emp|
+    #     #     return emp.salary if emp.employee.nil?
+    #     #     sal + emp.salary + emp_collect(emp)
+    #     # end
+    #     # sal
+    # end
 
 
 end
@@ -54,6 +68,6 @@ founder = Manager.new("ned", "founder", 1000000, [ta_m])
 
 
 
-founder.bonus(5,[ta_m]) # => 500_000
-ta_m.bonus(4,[e1,e2]) # => 88_000
+founder.bonus(5) # => 500_000
+ta_m.bonus(4) # => 88_000
 e2.bonus(3) # => 30_000
