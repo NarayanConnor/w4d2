96e382f07ba5f5335d5be768b431e4f3b8fee7c0
mordes89 <mordes89@gmail.com>
2021-07-06 14:52:08 -0400

    2:52

--- class_inher.rb
+++ class_inher.rb
@@ -4,7 +4,7 @@ class Employee
 
     attr_reader :name, :title, :salary, :boss
 
-    def initialize ( name, title,salary,boss)
+    def initialize (name, title, salary, boss)
         @name= name
         @title= title
         @salary=salary
@@ -19,16 +19,25 @@ end
 
 class Manager<Employee
 
-    def initialize(emp,bos=nil)
-        @employees=[emp]
-        @boss=boss
+    attr_writer :employees
+
+    def initialize(name, title, salary, boss=nil)
+        @employees=[]
+        super(name, title, salary, boss)
     end
 
    
     def bonus(multi)
-        #bonus = (total salary of all sub-employees) * multiplier
-    end        
-
-
+        # bonus = (total salary of all sub-employees) * multiplier
+    end
 
 end
+
+founder = Manager.new("ned", "founder", 1000000)
+to_m = Manager.new("darren", "ta manager", 1000000, founder)
+e1 = Employee.new("shawna", "ta", 12000, ta_m)
+e2 = Employee.new("david", "ta", 10000, ta_m)
+
+ned.bonus(5) # => 500_000
+darren.bonus(4) # => 88_000
+david.bonus(3) # => 30_000
