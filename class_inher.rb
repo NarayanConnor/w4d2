87cca4773a86794273e09c70454ba5437eb60939
Raboniel <imalwaysfantastic@gmail.com>
2021-07-06 14:32:03 -0400

    fried

--- class_inher.rb
+++ class_inher.rb
@@ -1,15 +1,34 @@
-<<<<<<< HEAD
 
 
 class Employee
 
+    attr_reader :name, :title, :salary, :boss
 
+    def initialize ( name, title,salary,boss)
+        @name= name
+        @title= title
+        @salary=salary
+        @boss=boss
+    end
 
+    def bonus(multi)
+        bonus=(salary)*multi
+    end
 
+end
+
+class Manager<Employee
+
+    def initialize(emp,bos=nil)
+        @employees=[emp]
+        @boss=boss
+    end
+
+   
+    def bonus(multi)
+        #bonus = (total salary of all sub-employees) * multiplier
+    end        
 
 
 
 end
-=======
-d
->>>>>>> eb6e55e30c326276b2e2dc07c7972ddd355e7ea6
