727f779664311e0e37e593f3ce7a7d29ebfa7467
mordes89 <mordes89@gmail.com>
2021-07-06 16:49:36 -0400

    449

--- chess/Board.rb
+++ chess/Board.rb
@@ -23,6 +23,9 @@ class Board
     end
 
     def move_piece(start_pos,end_pos)
+        riase if @board[start_pos[0]][start_pos[1]] == NullPiece
+        riase if @board[end_pos[0]][end_pos[1]] != NullPiece #if it's our own player
+        # not always true
     end
 
 end
