01306bd5bb64d3da89d4d7cd2ec9e77d37ae9992
Raboniel <imalwaysfantastic@gmail.com>
2021-07-07 14:18:44 -0400

    phrasing

---
+++ skeleton 2/user_script.rb
@@ -0,0 +1,11 @@
+require_relative 'super_useful'
+
+puts "'five' == #{convert_to_int(66)}"
+
+feed_me_a_fruit
+
+sam = BestFriend.new('gert', 9, '')
+
+sam.talk_about_friendship
+sam.do_friendstuff
+sam.give_friendship_bracelet
